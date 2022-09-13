a4b6a8257cc5dcf7cd124b857202d95556505daa
douglas <douglas.pereira@gft.com>
2022-09-13 09:05:58 -0300

    first commit

---
+++ Trade_Category/Trade_Category/Entity/Trade.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Trade_Category.Entity
+{
+    class Trade : ITrade
+    {
+        public double value;
+        public string clientSector;
+        public DateTime nextPaymentDate;
+
+        public double Value
+        {
+            get
+            {
+                return value;
+            }
+
+        }
+        public string ClientSector
+        {
+            get
+            {
+                return clientSector;
+            }
+
+        }
+        public DateTime NextPaymentDate
+        {
+            get
+            {
+                return nextPaymentDate;
+            }
+
+        }
+
+        public Trade()
+        {
+            value = 0;
+            clientSector = "";
+            nextPaymentDate = DateTime.Today;
+
+        }
+    }
+}
---
+++ Trade_Category/Trade_Category/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Trade_Category.Entity;
+
+namespace Trade_Category
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            List<Trade> ts = new List<Trade>();
+
+            DateTime aTime = new DateTime();
+            int quant = 0;
+
+
+            aTime = DateTime.ParseExact(Console.ReadLine(), "MM/dd/yyyy", CultureInfo.InvariantCulture);
+            quant = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < quant; i++)
+            {
+                Trade t = new Trade();
+                string a = Console.ReadLine();
+                string[] aList = a.Split(" ");
+
+                t.value = Double.Parse(aList[0]);
+                t.clientSector = aList[1];
+                t.nextPaymentDate = DateTime.ParseExact(aList[2], "MM/dd/yyyy", CultureInfo.InvariantCulture);
+                ts.Add(t);
+            }
+
+            foreach (Trade tr in ts)
+            {
+                TimeSpan tms = aTime.Subtract(tr.NextPaymentDate);
+                if (tr.Value > 1000000 && tr.ClientSector == "Public")
+                {
+                    Console.WriteLine("MEDIUMRISK");
+                }
+                if (tr.Value > 1000000 && tr.ClientSector == "Private")
+                {
+                    Console.WriteLine("HIGHRISK");
+                }
+                if (tms.Days > 30)
+                {
+                    Console.WriteLine("EXPIRED");
+                }
+                
+            }
+
+            //Console.WriteLine("Value: " + t.Value);
+            //Console.WriteLine("Sector: " + t.ClientSector);
+            //Console.WriteLine("Payment Date: " + t.NextPaymentDate);
+        }
+    }
+}
